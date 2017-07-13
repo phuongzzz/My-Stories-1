07499ad8cc3611d3ba5d5e0916e750dd209e79e1
Hoang Anh Tu <hoangtuyb96@gmail.com>
2017-07-13 15:51:02 +0700

    sign_up

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -3,6 +3,7 @@ require "api_constraints"
 namespace :api, defaults: {format: "json"} do
   devise_scope :user do
     post "sign_in", to: "sessions#create"
+    post "sign_up", to: "registrations#create"
     delete "sign_out", to: "sessions#destroy"
   end
 
