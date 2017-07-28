65274b99a75c85fe1eeab611a6356ec78242a6f0
DoanPhuc95 <phucdm1901@gmail.com>
2017-07-28 08:32:41 +0700

    create sub step

--- client/src/app/story/create/create.component.ts
+++ client/src/app/story/create/create.component.ts
@@ -1,75 +1,105 @@
 import { Component, OnInit } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CreateStoryService } from './create_story.service';
-import { Step } from './data-model';
 import { Router } from '@angular/router';
+import { TranslateService } from 'ng2-translate';
+import { MdSnackBar } from '@angular/material';
 import * as $ from 'jquery';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.scss'],
-  providers: [ FormBuilder, CreateStoryService ]
+  providers: [FormBuilder, CreateStoryService, MdSnackBar]
 })
 export class CreateComponent implements OnInit {
+  url_image_story = 'http://saveabandonedbabies.org/wp-content/uploads/2015/08/default.png';
   private current_user: any;
   StoryForm: FormGroup;
+  StepForm: FormArray;
   hidden = true;
+  sub_temp: number;
   private presentStep = -1;
   constructor(private formbuilder: FormBuilder, private router: Router,
-    private createService: CreateStoryService) {
+    private createService: CreateStoryService,
+    private snackBar: MdSnackBar, private translate: TranslateService) {
   }
 
   ngOnInit() {
     this.current_user = JSON.parse(localStorage.getItem('currentUser'));
     this.createForm();
     $('#story').addClass('animated fadeInLeft');
-    setInterval(function(){
+    setInterval(function() {
       $('#story').removeClass('animated fadeInLeft');
-    }, 1000);
+    }, 1500);
+  }
+
+  getStepForm(i: number): FormArray {
+    const step = <FormGroup> this.StepForm.controls[i];
+    return <FormArray>step.controls['sub_steps_attributes'];
   }
 
   createForm() {
     this.StoryForm = this.formbuilder.group({
-      name: '',
-      due_date: '',
+      name: ['', Validators.required],
+      due_date: ['', Validators.required],
       is_public: 'true',
-      description: '',
-      step_forms: this.formbuilder.array([
-        this.initStepForms()
-      ])
+      description: ['', Validators.required],
+      image: '',
+      step: this.formbuilder.array([])
     });
+    this.StepForm = <FormArray>this.StoryForm.controls['step'];
   }
 
   initStepForms() {
-    return this.formbuilder.group(new Step());
+    return this.formbuilder.group({
+      name: ['', Validators.required],
+      content: ['', Validators.required],
+      sub_steps_attributes: this.formbuilder.array([])
+    });
+  }
+
+  initSubStepForm() {
+    return this.formbuilder.group({
+      name: ['', Validators.required],
+      content: ['', Validators.required],
+      image: ''
+    })
   }
 
   createStep() {
+    this.StepForm.push(this.initStepForms());
+    this.presentStep = this.StepForm.length - 1;
     $('#story').slideUp('slow', function() {
       $('#step').fadeIn('slow');
     });
-    this.presentStep += 1;
     this.hidden = false;
   }
 
+  newSubStep() {
+    const control = <FormArray> this.getStepForm(this.presentStep);
+    control.push(this.initSubStepForm());
+  }
+
   newStep() {
+    window.scroll(0, 0);
     const step = '#step' + this.presentStep;
     $(step).addClass('animated fadeOutLeft');
     $(step).slideUp('slow');
-    const control = <FormArray>this.StoryForm.controls['step_forms'];
-    control.push(this.initStepForms());
-    this.presentStep = control.length -1;
-    setInterval(function(){
+    this.StepForm.push(this.initStepForms());
+    this.presentStep = this.StepForm.length - 1;
+    setInterval(function() {
       $(step).removeClass('animated fadeOutLeft');
-    }, 1000);
+    }, 1500);
   }
 
   next() {
-    console.log(this.presentStep);
     if (this.presentStep === -1) {
-      this.createStep();
-      return ;
+      $('#story').slideUp('slow', function() {
+        $('#step').fadeIn('slow');
+      });
+      this.presentStep += 1;
+      return;
     }
     const step = '#step' + this.presentStep;
     this.presentStep += 1;
@@ -77,9 +107,9 @@ export class CreateComponent implements OnInit {
     $(step_next).fadeIn();
     $(step).addClass('animated fadeOutLeft');
     $(step).slideUp('slow');
-    setInterval(function(){
+    setInterval(function() {
       $(step).removeClass('animated fadeOutLeft');
-    }, 1000);
+    }, 1500);
   }
 
   back() {
@@ -88,16 +118,16 @@ export class CreateComponent implements OnInit {
       $(step).addClass('animated fadeOutDown');
       $(step).fadeOut();
       $(step).removeClass('animated fadeOutDown');
-      setInterval(function(){
+      setInterval(function() {
         $(step).removeClass('animated fadeOutDown');
-      }, 1000);
+      }, 1500);
       this.presentStep -= 1;
       const step_back = '#step' + this.presentStep;
       $(step_back).fadeIn();
       $(step_back).addClass('animated fadeInLeft');
-      setInterval(function(){
-        $(step_back).removeClass('animated fadeInRight');
-      }, 1000);
+      setInterval(function() {
+        $(step_back).removeClass('animated fadeInLeft');
+      }, 1500);
       return;
     }
     $('#story').slideDown('slow', function() {
@@ -111,14 +141,27 @@ export class CreateComponent implements OnInit {
     textarea.style.height = '72px';
     const contentHeight = document.getElementById(id).scrollHeight;
     textarea.style.height = contentHeight + 'px';
-    window.scroll(0, contentHeight);
+  }
+
+  form_is_invalid(): boolean {
+    if (!this.StoryForm.dirty && !this.StoryForm.valid) {
+      return this.StoryForm.value.image === '';
+    }
+    return true;
   }
 
   submit() {
-    console.log(this.presentStep);
-    this.createService.createStory(this.StoryForm.value,
-      this.current_user.token).subscribe(response => this.onSuccess(response),
-      response => this.onError(response));
+    if (this.form_is_invalid()) {
+      this.snackBar.open( this.translate.instant('story_form.blank_noti') , '', {
+        duration: 5000
+      });
+      return;
+    } else {
+      this.createService.createStory(this.StoryForm.value,
+        this.current_user.token).subscribe(response => this.onSuccess(response),
+        response => this.onError(response));
+    }
+
   }
 
   onSuccess(response) {
@@ -131,8 +174,70 @@ export class CreateComponent implements OnInit {
         localStorage.removeItem('currentUser');
       }
       this.current_user = {};
-      sessionStorage.clear();
-      this.router.navigate(['login']);
+      this.router.navigate(['']);
     }
+  }
+
+  chooseImage(id: string) {
+    $(id).trigger('click');
+  }
+
+  onChange(e, id: number) {
+    if (e.target.files && e.target.files[0]) {
+      if (this.presentStep >= 0) {
+        this.sub_temp = id;
+      }
+      const reader = new FileReader();
+      reader.onload = this.changeImage.bind(this);
+      reader.readAsDataURL(e.target.files[0]);
+    }
+  }
+
+  changeImage(e) {
+    const image = <FileReader> e.target;
+    if (this.presentStep === -1) {
+      $('#story_cover').attr('src', image.result);
+      this.StoryForm.controls['image'].setValue(image.result);
+    } else {
+      const img = '#image' + this.presentStep + '_' + this.sub_temp;
+      $(img).attr('src', image.result);
+      const sub_step = <FormGroup>this.getStepForm(this.presentStep).controls[this.sub_temp];
+      sub_step.controls['image'].setValue(image.result);
+    }
+  }
+
+  delSubStep(id: number) {
+    const control = <FormArray> this.getStepForm(this.presentStep);
+    const sub = '#substep' + this.presentStep + '_' + id;
+    $(sub).fadeOut();
+    control.removeAt(id);
+  }
+
+  delStep() {
+    const step = '#step' + this.presentStep;
+    this.StepForm.removeAt(this.presentStep);
+    if (this.StepForm.length === 0) {
+      this.presentStep = -1;
+      this.hidden = true;
+      $('#step').fadeOut();
+      $('#story').slideDown('slow');
+      return;
+    }
+    if (this.presentStep === this.StepForm.length) {
+      this.presentStep -= 1;
+      const step_back = '#step' + this.presentStep;
+      $(step_back).fadeIn();
+      $(step_back).addClass('animated fadeInLeft');
+      setInterval(function() {
+        $(step_back).removeClass('animated fadeInLeft');
+      }, 1500);
+      return;
+    }
+    const step_next = '#step' + (this.presentStep +1);
+    $(step_next).fadeIn();
+    $(step_next).addClass('animated fadeInRight');
+    setInterval(function() {
+      $(step).removeClass('animated fadeOutRight');
+    }, 1500);
   }
 }
