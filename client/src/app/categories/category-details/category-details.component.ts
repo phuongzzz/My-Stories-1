6fea9e3117ed4bab7637530a2603fa714be07c95
Ebolaa <ebolaa@Ebolaas-MacBook-Pro-162.local>
2017-08-01 10:20:35 +0700

    home_ui_1

---
+++ client/src/app/categories/category-details/category-details.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import * as $ from 'jquery';
+
+@Component({
+  selector: 'app-category-details',
+  templateUrl: './category-details.component.html',
+  styleUrls: ['./category-details.component.css']
+})
+export class CategoryDetailsComponent implements OnInit {
+  category: any;
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
