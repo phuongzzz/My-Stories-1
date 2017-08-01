7110e5d97544deded32e0f288b817ec5e6a5a0f2
Ebolaa <ebolaa@Ebolaas-MacBook-Pro-162.local>
2017-08-01 09:00:38 +0700

    home_ui_3

---
+++ client/src/app/home/story.model.ts
@@ -0,0 +1,11 @@
+export class Story {
+  constructor(
+    public id: number,
+    public name_user: string,
+    public name: string,
+    public description: string,
+    public imageUrl: string
+  ) {
+
+  }
+}
