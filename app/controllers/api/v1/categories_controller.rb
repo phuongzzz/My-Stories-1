9f4e831fd28e8a43179713f0064cc25d63f2231d
hoangtuyb96 <hoangtuyb96@gmail.com>
2017-08-17 10:05:35 +0700

    add_kaminari

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -2,14 +2,19 @@ class Api::V1::CategoriesController < Api::BaseController
   skip_before_action :authenticate_user_from_token, only: :index
 
   def index
-    @categories = Category.all
+    @categories = Category.page(params[:page]).per params[:per_page]
     render json: {
       messages: I18n.t("categories.messages.categories_showed"),
-      data: {categories: categories}
+      data: {categories: categories_serializer}
     }, status: :ok
   end
 
   private
 
   attr_reader :categories
+
+  def categories_serializer
+    Serializers::Categories::CategorySerializer
+      .new(object: categories).serializer
+  end
 end
