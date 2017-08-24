d9179ecc8b6a6c99d21889d350e659da03c260ec
hoangtuyb96 <hoangtuyb96@gmail.com>
2017-08-24 17:06:05 +0700

    add_following_feed

--- app/models/serializers/users/user_serializer.rb
+++ app/models/serializers/users/user_serializer.rb
@@ -2,13 +2,28 @@ module Serializers
   module Users
     class UserSerializer < Serializers::SupportSerializer
       attrs :id, :email, :name, :avatar
-      attrs :stories
+      attrs :stories, :following_user, :following_story
 
       delegate :id, to: :object
 
       def stories
         Serializers::Stories::StorySerializer
           .new(object: object.stories.newest).serializer
+      end
+
+      def following_user
+        object.following
+      end
+
+      def following_story
+        tmp = []
+        tmp += object.following_story
+        object.following.each do |following_user|
+          tmp += following_user.stories
+        end
+        tmp = tmp.uniq
+        tmp = tmp.sort_by(&:updated_at).reverse
+        Serializers::Stories::StorySerializer.new(object: tmp).serializer
       end
     end
   end
